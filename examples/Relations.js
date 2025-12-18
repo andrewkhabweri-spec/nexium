4b728a56900247c744d3c2b6761ec846bb4be78b
andrewkhabweri-spec <andrewkhabweri@gmail.com>
2025-12-18 22:07:28 +0300

    Update Relations.js

--- examples/Relations.js
+++ examples/Relations.js
@@ -4,6 +4,8 @@ const posts = await user.posts().get();
 
 // Eager loading
 const usersWithPosts = await User.query().with('posts').get();
+// OR
+const usersloadPosts = await User.query().preload('posts').get();
 
 // Many-to-many
 const user = await User.find(2);
@@ -14,3 +16,5 @@ await user.roles().detach(3);
 
 // Eager load multiple relations
 const users = await User.query().with(['roles', 'posts', 'comments']).get();
+// OR
+const loadusers = await User.query().preload(['roles', 'posts', 'comments']).get();
